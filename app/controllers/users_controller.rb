a5783fc82627a2a238bf7622973976f2b2ee0785
angie <amaticorena103@hotmail.com>
2022-11-01 21:39:39 -0400

    Added Home component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    wrap_parameters
+rescue_from ActiveRecord:RecordInvalid, with: :record_invalid_response
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :first_name, :last_name, :phone_number, :email)
+    end
+
+    def record_invalid_response(user)
+        render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
+
 end
