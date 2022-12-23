9b03ea0feb3109f6a6b60eda7130c6eb0d92757d
angie <amaticorena103@hotmail.com>
2022-12-23 00:06:17 -0500

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_23_050200) do
+ActiveRecord::Schema.define(version: 2022_12_17_212136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2022_12_23_050200) do
     t.integer "vendor_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "current_cost"
+    t.float "total_cost"
   end
 
   create_table "events", force: :cascade do |t|
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_12_23_050200) do
     t.date "end_date"
     t.time "end_time"
     t.time "start_time"
+    t.float "current_cost"
   end
 
   create_table "users", force: :cascade do |t|
