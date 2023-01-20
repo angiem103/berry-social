f440fd1bc6ba0e69b31612696f9e0c1f399c4b59
angie <amaticorena103@hotmail.com>
2023-01-20 02:15:22 -0500

    update config

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_01_19_052604) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "phone_number"
