4dc119743e8b76896989d157535303ebc9dd99de
angie <amaticorena103@hotmail.com>
2023-01-19 23:39:18 -0500

    added password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :username, uniqueness: true, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 8 }
     validates :first_name, :last_name, :phone_number, :email, presence: true
 
     has_many :clients
